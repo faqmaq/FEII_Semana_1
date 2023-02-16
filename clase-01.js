7c2dade46cfdaaae94bb0591f857335ecdf19fe9
Faqmaq <famaqueda@gmail.com>
2023-02-15 22:27:57 -0300

    Semana 1 - Clase 02

--- clase-01.js
+++ clase-01.js
@@ -30,3 +30,4 @@ const nombreJugador = iniciarJuego();
 // 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
 // 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
 // 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
+
---
+++ clase-02.js
@@ -0,0 +1,92 @@
+console.log('Clase 02');
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 2                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Solicito la opción del usuario para jugar 1. Piedra 2. Papel y 3. Tijera
+// Validad que sea un número entre 1 y 3
+function pedirJugada(){
+    let opcion;
+
+    do {
+        opcion = parseInt( prompt('Ingrese 1.Piedra, 2.Papel o 3.Tijera') );
+    // Mientras no sea un número isNaN y opcion > 3 y opcion < 1
+    } while(  isNaN(opcion) || opcion > 3 || opcion < 1  )
+    
+    console.log('La opción es ' + opcion);
+    return opcion;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 3                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Obtengo un número randon entre 1 y 3 
+function jugadaRandom(){
+    let numero = parseInt( Math.random() * 3 + 1 );
+    console.log('La Compu Juega con ' + numero);
+    return numero;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 4                                 */
+/* -------------------------------------------------------------------------- */
+// 👇 Retorna el resultado del juego
+function compararJugada(){
+                            //           0                       1              2
+    const resultados_posible = ['¡Felicitaciones Ganaste!', 'Empate', 'Perdiste, Segui participando'];
+    const eleccionUsuario = pedirJugada();
+    const eleccionCompu = jugadaRandom();
+
+    let resultado;
+    //  1.Piedra 2.Papel y 3.Tijera
+    // Si empataron
+    if( eleccionCompu === eleccionUsuario  ){
+        resultado = resultados_posible[1];
+    } else if( 
+        ( eleccionUsuario === 1 && eleccionCompu === 2) ||
+        ( eleccionUsuario === 2 && eleccionCompu === 3) ||
+        ( eleccionUsuario === 3 && eleccionCompu === 1) 
+     ){
+        resultado = resultados_posible[2];
+    } else {
+        resultado = resultados_posible[0];
+    }
+
+    return resultado;
+}
+
+function recibirResultado(resultado){
+    console.log (resultado);
+    alert(resultado);
+    if( resultado === 'Perdiste, Segui participando'){
+        alert('Suerte en la próxima!!');       
+    }    
+    
+}
+
+function jugarDeNuevo(){
+    let confirmacion = confirm('¿Queres jugar otra vez?');
+    return confirmacion
+}
+
+let resultadoPartida;
+let confirmacion;
+
+do {
+
+    resultadoPartida = compararJugada();
+    recibirResultado(resultadoPartida);
+    confirmacion = jugarDeNuevo();
+} while (confirmacion === true)
+
+alert('El juego ha finalizado')
+
+
+/* -------------------------------------------------------------------------- */
+/*                          CONSIGNA MESA DE TRABAJO                          */
+/* -------------------------------------------------------------------------- */
+// 1- Crear una función que reciba como parametro un texto (la frase de resultado de la partida).
+// 2- La función debe mostrar por consola el resultado de la partida.
+// 3- A su vez debe mostrar al usuario una alerta con el resutado de la partida.
+// 4- Finalmente, si el resultado fue una derrota debe mostrarle al usuario un mensaje de aliento para desearle suerte en la próxima oportunidad.
+
